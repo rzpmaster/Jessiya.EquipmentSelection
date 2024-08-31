c4977f22bfffe18df3c62a8ef56cfdc3eb74bec1
zhipeng <rongzhipeng@outlook.com>
2024-08-31 20:33:11 +0800

    temp

--- AvaloniaUI/App.axaml.cs
+++ AvaloniaUI/App.axaml.cs
@@ -1,41 +1,54 @@
-﻿using Avalonia;
+﻿using System;
+using System.Linq;
+using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 
 using AvaloniaUI.ViewModels;
 using AvaloniaUI.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AvaloniaUI;
 
 public partial class App : Application
 {
+    private IServiceProvider? _provider;
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+
+        _provider = ConfigureServices();
     }
 
     public override void OnFrameworkInitializationCompleted()
     {
-        // Line below is needed to remove Avalonia data validation.
-        // Without this line you will get duplicate validations from both Avalonia and CT
-        BindingPlugins.DataValidators.RemoveAt(0);
-
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainViewModel()
-            };
-        }
-        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
-        {
-            singleViewPlatform.MainView = new MainView
-            {
-                DataContext = new MainViewModel()
-            };
+            var mainViewModel = _provider?.GetRequiredService<MainViewModel>();
+            var mainView = _provider?.GetRequiredService<MainWindow>();
+            mainView!.DataContext = mainViewModel;
+            desktop.MainWindow = mainView;
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private IServiceProvider? ConfigureServices()
+    {
+        var services = new ServiceCollection();
+
+        // Views
+        services.AddSingleton<MainWindow>();
+
+        // ViewModels
+        services.AddSingleton<MainViewModel>();
+        var types = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(s => s.GetTypes())
+            .Where(p => !p.IsAbstract && typeof(IViewModel).IsAssignableFrom(p));
+        foreach (var type in types)
+            services.AddSingleton(typeof(IViewModel), type);
+
+        return services.BuildServiceProvider();
     }
 }
--- AvaloniaUI/ViewModels/ViewModelBase.cs
+++ AvaloniaUI/ViewModels/ViewModelBase.cs
@@ -2,6 +2,6 @@
 
 namespace AvaloniaUI.ViewModels;
 
-public class ViewModelBase : ObservableObject
+public class ViewModelBase : ObservableObject, IViewModel
 {
 }
