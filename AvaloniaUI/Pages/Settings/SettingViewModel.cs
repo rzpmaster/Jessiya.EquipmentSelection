b73ce215ec405e445d092252c7c5bf0943b3006f
zhipeng <rongzhipeng@outlook.com>
2024-09-01 12:46:27 +0800

    settings page

--- AvaloniaUI/Pages/Settings/SettingViewModel.cs
+++ AvaloniaUI/Pages/Settings/SettingViewModel.cs
@@ -3,11 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Avalonia.Collections;
+using Avalonia.Styling;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Material.Icons;
+using SukiUI.Enums;
+using SukiUI.Models;
+using SukiUI;
 
 namespace AvaloniaUI.Pages.Settings
 {
-    public partial class SettingViewModel() : PageBase("Settings", MaterialIconKind.Settings, 100)
+    public partial class SettingViewModel : PageBase
     {
+        public Action<SukiBackgroundStyle>? BackgroundStyleChanged { get; set; }
+        public Action<bool>? BackgroundAnimationsChanged { get; set; }
+        public Action<bool>? BackgroundTransitionsChanged { get; set; }
+        public Action<string?>? CustomBackgroundStyleChanged { get; set; }
+
+        public IAvaloniaReadOnlyList<SukiColorTheme> AvailableColors { get; }
+        public IAvaloniaReadOnlyList<SukiBackgroundStyle> AvailableBackgroundStyles { get; }
+        public IAvaloniaReadOnlyList<string> CustomShaders { get; } = new AvaloniaList<string> { "Space", "Weird", "Clouds" };
+
+        private readonly SukiTheme _theme = SukiTheme.GetInstance();
+
+        [ObservableProperty] private bool _isLightTheme;
+        [ObservableProperty] private SukiBackgroundStyle _backgroundStyle;
+        [ObservableProperty] private bool _backgroundAnimations;
+        [ObservableProperty] private bool _backgroundTransitions;
+
+        private string? _customShader = null;
+
+        public SettingViewModel() : base("Settings", MaterialIconKind.Settings, 100)
+        {
+            AvailableBackgroundStyles = new AvaloniaList<SukiBackgroundStyle>(Enum.GetValues<SukiBackgroundStyle>());
+            AvailableColors = _theme.ColorThemes;
+            IsLightTheme = _theme.ActiveBaseTheme == ThemeVariant.Light;
+            _theme.OnBaseThemeChanged += variant =>
+                IsLightTheme = variant == ThemeVariant.Light;
+            _theme.OnColorThemeChanged += theme =>
+            {
+                // TODO: Implement a way to make this correct, might need to wrap the thing in a VM, this isn't ideal.
+            };
+        }
+
+        partial void OnIsLightThemeChanged(bool value) =>
+            _theme.ChangeBaseTheme(value ? ThemeVariant.Light : ThemeVariant.Dark);
+
+        [RelayCommand]
+        private void SwitchToColorTheme(SukiColorTheme colorTheme) =>
+            _theme.ChangeColorTheme(colorTheme);
+
+        partial void OnBackgroundStyleChanged(SukiBackgroundStyle value) =>
+            BackgroundStyleChanged?.Invoke(value);
+
+        partial void OnBackgroundAnimationsChanged(bool value) =>
+            BackgroundAnimationsChanged?.Invoke(value);
+
+        partial void OnBackgroundTransitionsChanged(bool value) =>
+            BackgroundTransitionsChanged?.Invoke(value);
+
+        [RelayCommand]
+        private void TryCustomShader(string shaderType)
+        {
+            _customShader = _customShader == shaderType ? null : shaderType;
+            CustomBackgroundStyleChanged?.Invoke(_customShader);
+        }
     }
 }
--- AvaloniaUI/Views/MainViewModel.cs
+++ AvaloniaUI/Views/MainViewModel.cs
@@ -1,33 +1,45 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Avalonia.Collections;
+using Avalonia.Styling;
 using AvaloniaUI.Pages;
+using AvaloniaUI.Pages.Settings;
 using AvaloniaUI.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
+using SukiUI;
 using SukiUI.Dialogs;
+using SukiUI.Enums;
+using SukiUI.Models;
 using SukiUI.Toasts;
 
 namespace AvaloniaUI.Views;
 
 public partial class MainViewModel : ObservableObject
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public IAvaloniaReadOnlyList<PageBase> Pages { get; }
+    public IAvaloniaReadOnlyList<SukiColorTheme> Themes { get; }
+    //public IAvaloniaReadOnlyList<SukiBackgroundStyle> BackgroundStyles { get; }
+
     public ISukiToastManager ToastManager { get; }
     public ISukiDialogManager DialogManager { get; }
+
+    private readonly SukiTheme _theme;
+    //private readonly SettingViewModel _setting;
 
     public MainViewModel(IEnumerable<PageBase> demoPages, PageNavigationService pageNavigationService, ISukiToastManager toastManager, ISukiDialogManager dialogManager)
     {
         ToastManager = toastManager;
         DialogManager = dialogManager;
         Pages = new AvaloniaList<PageBase>(demoPages.OrderBy(x => x.Index).ThenBy(x => x.DisplayName));
-        //_theming = (ThemingViewModel)DemoPages.First(x => x is ThemingViewModel);
-        //_theming.BackgroundStyleChanged += style => BackgroundStyle = style;
-        //_theming.BackgroundAnimationsChanged += enabled => AnimationsEnabled = enabled;
-        //_theming.CustomBackgroundStyleChanged += shader => CustomShaderFile = shader;
-        //_theming.BackgroundTransitionsChanged += enabled => TransitionsEnabled = enabled;
+        //_setting = (SettingViewModel)Pages.First(x => x is SettingViewModel);
+        //_setting.BackgroundStyleChanged += style => BackgroundStyle = style;
+        //_setting.BackgroundAnimationsChanged += enabled => AnimationsEnabled = enabled;
+        //_setting.CustomBackgroundStyleChanged += shader => CustomShaderFile = shader;
+        //_setting.BackgroundTransitionsChanged += enabled => TransitionsEnabled = enabled;
 
         //BackgroundStyles = new AvaloniaList<SukiBackgroundStyle>(Enum.GetValues<SukiBackgroundStyle>());
-        //_theme = SukiTheme.GetInstance();
+        _theme = SukiTheme.GetInstance();
 
         // Subscribe to the navigation service (when a page navigation is requested)
         pageNavigationService.NavigationRequested += pageType =>
@@ -37,28 +49,30 @@ public partial class MainViewModel : ObservableObject
             ActivePage = page;
         };
 
-        //Themes = _theme.ColorThemes;
-        //BaseTheme = _theme.ActiveBaseTheme;
+        Themes = _theme.ColorThemes;
+        BaseTheme = _theme.ActiveBaseTheme;
 
-        //// Subscribe to the base theme changed events
-        //_theme.OnBaseThemeChanged += variant =>
-        //{
-        //    BaseTheme = variant;
-        //    ToastManager.CreateSimpleInfoToast()
-        //        .WithTitle("Theme Changed")
-        //        .WithContent($"Theme has changed to {variant}.")
-        //        .Queue();
-        //};
+        // Subscribe to the base theme changed events
+        _theme.OnBaseThemeChanged += variant =>
+        {
+            BaseTheme = variant;
+            ToastManager.CreateSimpleInfoToast()
+                .WithTitle("Theme Changed")
+                .WithContent($"Theme has changed to {variant}.")
+                .Queue();
+        };
 
-        //// Subscribe to the color theme changed events
-        //_theme.OnColorThemeChanged += theme => ToastManager.CreateSimpleInfoToast()
-        //    .WithTitle("Color Changed")
-        //    .WithContent($"Color has changed to {theme.DisplayName}.")
-        //    .Queue();
+        // Subscribe to the color theme changed events
+        _theme.OnColorThemeChanged += theme => ToastManager.CreateSimpleInfoToast()
+            .WithTitle("Color Changed")
+            .WithContent($"Color has changed to {theme.DisplayName}.")
+            .Queue();
     }
 
-    public IAvaloniaReadOnlyList<PageBase> Pages { get; }
-
-    [ObservableProperty]
-    private PageBase? _activePage;
+    [ObservableProperty] private ThemeVariant _baseTheme;
+    [ObservableProperty] private PageBase? _activePage;
+    //[ObservableProperty] private SukiBackgroundStyle _backgroundStyle = SukiBackgroundStyle.Bubble;
+    //[ObservableProperty] private bool _animationsEnabled;
+    //[ObservableProperty] private string? _customShaderFile;
+    //[ObservableProperty] private bool _transitionsEnabled;
 }
